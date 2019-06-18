a9daeed528f69fd8fef21341fcae488ed28eea73
WillyWax <williamtrans2011@gmail.com>
2019-06-18 16:29:18 +0200

    [Starts

---
+++ services/connection.js
@@ -0,0 +1,13 @@
+const { Client, Pool } = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectionString =
+  process.env.ENV === "testing"
+    ? process.env.TEST_URL
+    : process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString: connectionString });
+
+module.exports = pool;
