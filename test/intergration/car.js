abeb6f0d73b5e5c8be61cb2d6086ce7edddbfb30
WillyWax <williamtrans2011@gmail.com>
2019-06-20 05:25:51 +0200

    ch(cars) Search car end point Tests and Swagger Documentation update
    add tests to check getting all cars
    add test to check getting car by id successfully
    add test to check trying to get car by wrong id
    add test to find cars using query parameters. By using state, status, price range, model, manufacturer
    add swagger documentation to simulate end point response
    
    [Starts #166810058] [Finishes #166810058]

--- test/intergration/car.js
+++ test/intergration/car.js
@@ -1,6 +1,7 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
+const assert = require("assert");
 
 chai.use(chaiHttp);
 const url = "http://localhost:3000/api/v1";
@@ -68,6 +69,7 @@ describe("Testing Cars Enpoints", () => {
         done();
       });
   });
+
   it("Creates car as authenticated User", done => {
     let carObject = {
       state: "used",
@@ -105,6 +107,89 @@ describe("Testing Cars Enpoints", () => {
       .send(carObject)
       .end((err, res) => {
         expect(res).to.have.status(422);
+
+        done();
+      });
+  });
+
+  it("Get Cars", done => {
+    requester
+      .get("/car")
+      .send()
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data.length).greaterThan(0);
+
+        done();
+      });
+  });
+
+  it("Should get unsold cars ", done => {
+    requester
+      .get("/car?status=available")
+      .send()
+      .end((err, res) => {
+        let data = res.body.data;
+        assert.notEqual(data.length, 0);
+
+        done();
+      });
+  });
+
+  it("Should get unsold cars by using price range ", done => {
+    requester
+      .get("/car?status=available&min_price=50000&max_price=57000")
+      .send()
+      .end((err, res) => {
+        let data = res.body.data;
+        assert.equal(data.length, 1);
+
+        done();
+      });
+  });
+
+  it("Should filter by either manufacturer, model or year if car exists", done => {
+    requester
+      .get("/car?manufacturer=Jeep")
+      .send()
+      .end((err, res) => {
+        let data = res.body.data;
+        assert.equal(data.length, 1);
+
+        done();
+      });
+  });
+
+  it("Should return empty car not found when wrong car Id", done => {
+    requester
+      .get("/car/123")
+      .send()
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+
+        done();
+      });
+  });
+
+  it("Should Fail to filter if wrong query provided", done => {
+    requester
+      .get("/car?make=Jeep")
+      .send()
+      .end((err, res) => {
+        let data = res.body.data;
+        assert.equal(data.length, 0);
+        expect(res).to.have.status(404);
+
+        done();
+      });
+  });
+
+  it("Get Car By Id", done => {
+    requester
+      .get("/car/" + carId)
+      .send()
+      .end((err, res) => {
+        expect(res).to.have.status(200);
 
         done();
       });
